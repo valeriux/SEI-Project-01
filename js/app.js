8ce95d2bb08cab3bbdb03bc7bbdf908f5f708bd3
Valeria Bocanegra <valeriliux@icloud.com>
2019-04-01 10:38:32 +0100

    Project1 aliens added

--- js/app.js
+++ js/app.js
@@ -1,58 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  //Creating my Board "wrap" 13*13.
   const wrap = document.querySelector('.wrap')
   const width = 13
   const squares = []
-  let playerIndex = 162
-  let direction = 'forward'
-  let currentStep = 0
 
+  //This for is to create my div tags.
   for(let i = 0; i < width * width; i++) {
-    const square = document.createElement('DIV')
+    const square = document.createElement('div')
     squares.push(square)
     wrap.appendChild(square)
   }
 
+  //Index for my player.
+  let playerIndex = 162
   squares[playerIndex].classList.add('player')
-  squares[playerIndex].setAttribute('data-direction', direction)
-  squares[playerIndex].setAttribute('data-step', currentStep)
+
 
   function move() {
-    // update the current step (should count from 0 - 3, then start again)
-    currentStep = currentStep === 3 ? 0 : currentStep + 1
-    // find the square with the class of "player"
+    // Find the square with the class of "player"
     const player = squares.find(square => square.classList.contains('player'))
-    // remove the class of player from that square
+    // Remove the class of player from that square
     player.classList.remove('player')
-
-    // add the class of player to square the player should move to
+    // Add the class of player to square the player should move to
     squares[playerIndex].classList.add('player')
-    // update the direction (for styling)
-    squares[playerIndex].setAttribute('data-direction', direction)
-    // update the current step (for styling)
-    squares[playerIndex].setAttribute('data-step', currentStep)
   }
+
 
   document.addEventListener('keydown', (e) => {
     switch(e.keyCode) {
       case 37:
-        // left
+        // Left
         if(playerIndex % width > 0) {
           playerIndex--
-          direction = 'backward'
           move()
         }
         break
 
       case 39:
-        // right
+        // Right
         if(playerIndex % width < width - 1) {
           playerIndex++
-          direction = 'forward'
           move()
         }
         break
-
     }
   })
+
+  // Aliens//
+  let aliensArray = [1, 4, 7, 10,
+    13, 16, 19, 22,
+    25, 28, 31, 34]
+
+  aliensArray.forEach(aliens => {
+    squares[aliens].classList.add('aliens')
+  })
+
+  setInterval(() => {
+    aliensArray.forEach(aliens => {
+      squares[aliens].classList.remove('aliens')
+    })
+    aliensArray = aliensArray.map(aliens => aliens + 1)
+
+    aliensArray.forEach(aliens => {
+      squares[aliens].classList.add('aliens')
+    })
+  }, 1000)
+
+
+  //Movement//
+  let movement = []
+
+
+  function createLaser() {
+    const element = document.createElement('img')
+    element.src = 'img/laser-blue-1.png'
+    element.className = 'laser'
+
+  }
+
+
 })
