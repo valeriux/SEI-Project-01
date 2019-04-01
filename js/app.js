dfd34ca27caf7f59c35be149463909f3ffe2f80a
Valeria Bocanegra <valeriliux@icloud.com>
2019-04-01 14:37:25 +0100

    Project-01 missiles added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let playerIndex = 162
   squares[playerIndex].classList.add('player')
 
-
   function move() {
     // Find the square with the class of "player"
     const player = squares.find(square => square.classList.contains('player'))
@@ -25,7 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add the class of player to square the player should move to
     squares[playerIndex].classList.add('player')
   }
+  //Index for my missiles.
 
+  function shootMissiles() {
+    // get the index of the square above the player
+    let missilesIndex = playerIndex - width
+    // get the DOM element of the square above the player
+    let missiles = squares[missilesIndex]
+    // Add the class of missiles to that square
+    missiles.classList.add('missiles')
+    // Move missiles up
+    const missilesInterval = setInterval(() => {
+      console.log(missilesIndex)
+      // Remove missiles class from the current square
+      missiles.classList.remove('missiles')
+      if (missilesIndex - width < 0) clearInterval(missilesInterval)
+      else {// Set the new index for the missiles square
+        missilesIndex -= width
+        // Get the new DOM element of the next square
+        missiles = squares[missilesIndex]
+        // Add missiles class to the next square up
+        missiles.classList.add('missiles')
+      }
+      //...repeat every 100ms
+    }, 100)
+  }
+  //Next step: Remove when I got shoot
 
   document.addEventListener('keydown', (e) => {
     switch(e.keyCode) {
@@ -44,40 +68,44 @@ document.addEventListener('DOMContentLoaded', () => {
           move()
         }
         break
+
+      case 32:
+        // Laser
+        shootMissiles()
     }
   })
 
-  // Aliens//
+  // Aliens Array//
   let aliensArray = [1, 4, 7, 10,
     13, 16, 19, 22,
-    25, 28, 31, 34]
+    25, 28, 31, 34,
+    37, 40, 43, 46]
 
-  aliensArray.forEach(aliens => {
-    squares[aliens].classList.add('aliens')
-  })
-
-  setInterval(() => {
-    aliensArray.forEach(aliens => {
-      squares[aliens].classList.remove('aliens')
-    })
-    aliensArray = aliensArray.map(aliens => aliens + 1)
-
+  function createAliens(){
     aliensArray.forEach(aliens => {
       squares[aliens].classList.add('aliens')
     })
-  }, 1000)
-
-
-  //Movement//
-  let movement = []
-
-
-  function createLaser() {
-    const element = document.createElement('img')
-    element.src = 'img/laser-blue-1.png'
-    element.className = 'laser'
-
   }
+
+  createAliens()
+  let moveIndex=0
+  let moves=[1, 13, -1, -13]
+
+
+  function moveAliens(movement) {
+    aliensArray.forEach(aliens => {
+      squares[aliens].classList.remove('aliens')
+    })
+    aliensArray = aliensArray.map(aliens => aliens + movement)
+    aliensArray.forEach(aliens => {
+      squares[aliens].classList.add('aliens')
+    })
+  }
+
+  setInterval(() => {
+    moveIndex= moveIndex === 3 ? 0 : moveIndex + 1
+    moveAliens(moves[[moveIndex]])
+  }, 800)
 
 
 })
