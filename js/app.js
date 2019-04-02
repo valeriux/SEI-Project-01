73d78a11de838da81ff749f9d063f8d2b7f4df09
Valeria Bocanegra <valeriliux@icloud.com>
2019-04-02 10:50:34 +0100

    Project-01 aliens array aliens restart when I pressed start button

--- js/app.js
+++ js/app.js
@@ -1,54 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  //Creating my Board "wrap" 13*13.
+  ///////////////Creating my div tags my board name is "wrap" 13*13 ////////////
   const wrap = document.querySelector('.wrap')
   const width = 13
   const squares = []
-  const missileAudio = new Audio('sound/sfx-laser1.ogg')
-  let alienMoveInterval = null
-  const scoreBoard      = document.querySelector('.score')
-  const btnStart        = document.querySelector('button')
+  //////////////////////////////////////////////////////////////////////////////
+  const missileAudio = document.querySelector('audio')
+  const btnStart        = document.querySelector('.button')
   const secondScreen    = document.querySelector('.second_screen')
+  const scoreBoard      = document.querySelector('.score')
   const showFinalScore  = document.querySelector('.show-final-score')
 
-
+  const moves=[1, 13, -1, -13]
+  let moveIndex = 0
   let score = 0
   let timeUp = false
-  //When btnStart is click I activate my class start.
-  btnStart.addEventListener('click', start)
+  let alienMoveInterval = null
 
-  //Function activated when I click my btnStart.
-  function start() {
-    scoreBoard.textContent = score
-    scoreBoard.classList.remove('add')
-    secondScreen.classList.add('hide')//Hide the secondScreen when the time is end.
-    // Initializing music
-    //music()//Invoking function music
-    setTimeout(() => {//Setting my timeout when my time is finish.
-      timeUp = true
-      secondScreen.classList.remove('hide')//The time is end when the timeup = true and remove my secondScreen.
-      //My condition if score is >0 showFinalScore added in my Welcome screen.
-      if (score > 0) {//If score > 0 only show final score.
-        showFinalScore.classList.add('show')
-        //If score>8 show Excellent message.
-        const message = 'Your score is: ' + score + (score >= 20 ? ' That is a very good score!': '')
-        showFinalScore.textContent = message
-      }
-    }, 10000)// For 60 seconds = 60000 miliseconds.
-  }
-
-
-  //This for is to create my div tags.
+  //////////////This for is to create my div tags on my wrap////////////////////
   for(let i = 0; i < width * width; i++) {
     const square = document.createElement('div')
     squares.push(square)
     wrap.appendChild(square)
   }
 
-  //Index for my player.
+  //Index for my player position 162
   let playerIndex = 162
   squares[playerIndex].classList.add('player')
 
+  //Creating my function move to take the position of my player.
   function move() {
     // Find the square with the class of "player"
     const player = squares.find(square => square.classList.contains('player'))
@@ -57,6 +37,24 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add the class of player to square the player should move to
     squares[playerIndex].classList.add('player')
   }
+
+  function alienWasShot(missilesIndex, missilesInterval) {
+    // //Remove Missiles class.
+    squares[missilesIndex].classList.remove('missiles')
+    scoreBoard.classList.add('add')
+    score++
+    scoreBoard.textContent = score
+    clearInterval(missilesInterval)
+    const index = aliensArray.indexOf(missilesIndex)
+    console.log(missilesIndex)
+    //Splice the index from the div in array.
+    aliensArray.splice(index,1)
+    // //Remove Alien class.
+    squares[missilesIndex].classList.remove('aliens')
+    squares[missilesIndex].classList.remove('missiles')
+    //console.log(aliens)
+  }
+
   //Index for my missiles.
   function shootMissiles() {
     // Get the index of the square above the player
@@ -79,29 +77,15 @@ document.addEventListener('DOMContentLoaded', () => {
         // Add missiles class to the next square up
         missiles.classList.add('missiles')
       }
-      /////////////////Remove when Alien got shot///////////////////////
+      /////////////////Remove my aliens when they Alien got shot////////////////
       //Clear missilesInterval//
       if(squares[missilesIndex].classList.contains('aliens')){
-        // //Remove Missiles class.
-        squares[missilesIndex].classList.remove('missiles')
-        scoreBoard.classList.add('add')
-        score++
-        scoreBoard.textContent = score
-        clearInterval(missilesInterval)
-        const index = aliensArray.indexOf(missilesIndex)
-        console.log(missilesIndex)
-        //Splice the index from the div in array.
-        aliensArray.splice(index,1)
-        // //Remove Alien class.
-        squares[missilesIndex].classList.remove('aliens')
-        squares[missilesIndex].classList.remove('missiles')
-        //console.log(aliens)
-
+        alienWasShot(missilesIndex, missilesInterval)
       }
-      //Repeat every 60ms//
-    }, 60)
+    }, 60)//Repeat every 60ms//
   }
 
+  //Creating my cases for Left, right and laser
   document.addEventListener('keydown', (e) => {
     switch(e.keyCode) {
       case 37:
@@ -126,24 +110,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
-  // Aliens Array//
-  let aliensArray = [1, 4, 7, 10,
-    13, 16, 19, 22,
-    25, 28, 31, 34,
-    37, 40, 43, 46,
-    49, 52, 55, 58,
-    61, 64, 67, 70]
+  // Creating my Aliens Array//
+  let aliensArray = []
 
+  // Function to create my aliens.
   function createAliens(){
-    aliensArray.forEach(aliens => {
-      squares[aliens].classList.add('aliens')
+    squares.forEach((square, index) => {
+      square.classList.remove('aliens')
+      if(aliensArray.includes(index)) square.classList.add('aliens')
     })
   }
-
-  createAliens()
-  let moveIndex=0
-  let moves=[1, 13, -1, -13]
-
 
   function moveAliens(movement) {
     aliensArray.forEach(aliens => {
@@ -155,9 +131,44 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  alienMoveInterval = setInterval(() => {
-    moveIndex= moveIndex === 3 ? 0 : moveIndex + 1
-    moveAliens(moves[[moveIndex]])
-  }, 500)
+  //Function activated when I click my btnStart.
+  function start() {
+    clearInterval(alienMoveInterval)
+    aliensArray = [
+      1, 4, 7, 10,
+      13, 16, 19, 22,
+      25, 28, 31, 34,
+      37, 40, 43, 46,
+      49, 52, 55, 58,
+      61, 64, 67, 70,
+      73, 76, 79, 82
+    ]
+    createAliens()
+    moveIndex = 0
+    alienMoveInterval = setInterval(() => {
+      moveIndex= moveIndex === 3 ? 0 : moveIndex + 1
+      moveAliens(moves[moveIndex])
+    }, 500)//Interval time for my aliens.
 
+    score=0
+    scoreBoard.textContent = score
+    timeUp = false
+    scoreBoard.classList.remove('add')
+    secondScreen.classList.add('hide')//Hide the secondScreen when the time is end.
+    // Initializing music
+    setTimeout(() => {//Setting my timeout when my time is finish.
+      timeUp = true
+      secondScreen.classList.remove('hide')//The time is end when the timeup = true and remove my secondScreen.
+      //My condition if score is >0 showFinalScore added in my Welcome screen.
+      if (score > 0) {//If score > 0 only show final score.
+        showFinalScore.classList.add('show')
+        //If score>8 show Excellent message.
+        const message = 'Your score is: ' + score + (score >= 8 ? ' You score is good!': '')
+        showFinalScore.textContent = message
+      }
+      clearInterval(alienMoveInterval)
+    }, 9000)
+  }
+  //When btnStart is click I activate my class start.
+  btnStart.addEventListener('click', start)
 })
